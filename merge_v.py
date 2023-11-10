1d3e4e3db4a9a59b5282ab98eec338936429940f
tzy <theanzy1@gmail.com>
2023-11-10 14:12:14 +0800

    scripts

---
+++ merge_v.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+from PIL import Image
+
+
+def merge_frames_v(images: list[Image.Image]) -> Image.Image:
+    widths = map(lambda img: img.width, images)
+    img_w = max(widths)
+    img_h = images[0].height
+    spritesheet_width = img_w
+    spritesheet_height = img_h * len(images)
+    spritesheet = Image.new(
+        'RGBA', (int(spritesheet_width), int(spritesheet_height)))
+    print(spritesheet.width, spritesheet.height)
+
+    for i, frame in enumerate(images):
+        left = 0
+        right = frame.width
+        top = i * frame.height
+        bottom = top + frame.height
+        box = (left, top, right, bottom)
+        cut_frame = frame.crop((0, 0, frame.width, frame.height))
+        print('i', i)
+        spritesheet.paste(cut_frame, box)
+
+    return spritesheet
+
+
+def get_frames(dir: str) -> list[Image.Image]:
+    frames = []
+    files = os.listdir(dir)
+    for name in files:
+        filepath = os.path.join(dir, name)
+        try:
+            img = Image.open(filepath)
+            frames.append(img)
+        except:
+            print(f'{filepath} is not a valid image')
+    return frames
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Images to spritesheet.')
+    parser.add_argument('path', type=str,
+                        help='path to image')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='output path')
+    args = parser.parse_args()
+
+    dirpath = args.path
+    outpath = args.output
+
+    frames = get_frames(dirpath)
+    sprite = merge_frames_v(frames)
+    sprite.save(outpath, "PNG")
+
+
+if __name__ == '__main__':
+    main()
---
+++ spritesheet.py
@@ -0,0 +1,64 @@
+from PIL import Image
+import os
+import math
+import time
+import argparse
+
+
+max_sprites_row = 3.0
+frames = []
+
+
+def save_image():
+    return
+
+
+def merge_frames(frames: list[Image.Image]) -> Image.Image:
+
+    tile_width = frames[0].width
+    tile_height = frames[0].height
+    spritesheet_width = tile_width * len(frames)
+    spritesheet_height = tile_height
+    print('ss', spritesheet_width)
+    spritesheet = Image.new(
+        "RGBA", (int(spritesheet_width), int(spritesheet_height)))
+    spritesheet.show()
+    for i, frame in enumerate(frames):
+        left = i * tile_width
+        right = left + tile_width
+        top = 0
+        bottom = top + tile_height
+        box = (left, top, right, bottom, )
+        cut_frame = frame.crop((0, 0, tile_width, tile_height))
+        spritesheet.paste(cut_frame, box)
+
+    return spritesheet
+
+
+def get_frames(dir: str) -> list[Image.Image]:
+    frames = []
+    files = os.listdir(dir)
+    for name in files:
+        filepath = os.path.join(dir, name)
+        try:
+            img = Image.open(filepath)
+            frames.append(img)
+        except:
+            print(f'{filepath} is not a valid image')
+    return frames
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Images to spritesheet.')
+    parser.add_argument('path', type=str,
+                        help='directory containing frames')
+    args = parser.parse_args()
+    frames = get_frames(args.path)
+    spritesheet = merge_frames(frames)
+    spritesheet.save('spritesheet.png', "PNG")
+    for f in frames:
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
